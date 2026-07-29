5c7f35669c1260b3af93095d127e1ec74e33deda
Fihaonantsoa <fihaonantsoacgm@gmail.com>
2026-07-29 17:04:08 +0300

    test

--- api/index.js
+++ api/index.js
@@ -1,23 +1,37 @@
 // api/index.js
 import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { errorHandler } from '../src/middlewares/error.middleware.js';
-import routes from '../src/routes/index.js';
-
-dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/v1', routes);
-
 app.get('/api/health', (req, res) => {
-  res.status(200).json({ status: 'OK', message: 'API fonctionnelle sur Vercel' });
+  res.json({ status: 'OK' });
 });
 
-app.use(errorHandler);
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Test réussi' });
+});
 
 export default app;
+// // api/index.js
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import { errorHandler } from '../src/middlewares/error.middleware.js';
+// import routes from '../src/routes/index.js';
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/api/v1', routes);
+
+// app.get('/api/health', (req, res) => {
+//   res.status(200).json({ status: 'OK', message: 'API fonctionnelle sur Vercel' });
+// });
+
+// app.use(errorHandler);
+
+// export default app;
